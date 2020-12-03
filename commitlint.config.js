bf4e38c62fa3f21666ede0a05b2e5426ad3aa0a4
chaizhize <chaizhize>
2020-12-03 16:22:16 +0800

    docs: 3123

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-12-03 16:04:58
- * @LastEditTime: 2020-12-03 16:15:17
+ * @LastEditTime: 2020-12-03 16:20:38
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /vue-goodsku/commitlint.config.js
@@ -12,14 +12,16 @@ module.exports = {
         "type-enum": [
             2, //代表必须输入
             "always", [
-                "upd" | "update",
-                "feat",
-                "fix",
-                "refactor",
-                "docs",
-                "chore",
-                "style",
-                "revert",
+                "upd", // 更新某功能（不是 feat, 不是 fix）
+                "feat", // 新功能（feature）
+                "fix", // 修补bug
+                "refactor", // 重构（即不是新增功能，也不是修改bug的代码变动）
+                "docs", //修改了文档，比如README等
+                "chore", //构建过程或辅助工具的变动
+                "style", // 格式（不影响代码运行的变动）
+                "revert", // 回滚到上一个版本
+                "test", //增加测试
+                "perf", // 优化相关，比如提升性能、体验
             ],
         ],
         "type-case": [0],
