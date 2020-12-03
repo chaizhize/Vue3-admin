2614339dda96976a3c59559b09f7c090b292d021
chaizhize <chaizhize>
2020-12-03 16:29:21 +0800

    docs: 3123

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-12-03 16:04:58
- * @LastEditTime: 2020-12-03 16:20:38
+ * @LastEditTime: 2020-12-03 16:28:32
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /vue-goodsku/commitlint.config.js
@@ -12,7 +12,7 @@ module.exports = {
         "type-enum": [
             2, //代表必须输入
             "always", [
-                "upd", // 更新某功能（不是 feat, 不是 fix）
+                "upd", // 更新某功能（不是 feat, 不是 fix）1
                 "feat", // 新功能（feature）
                 "fix", // 修补bug
                 "refactor", // 重构（即不是新增功能，也不是修改bug的代码变动）
@@ -20,16 +20,16 @@ module.exports = {
                 "chore", //构建过程或辅助工具的变动
                 "style", // 格式（不影响代码运行的变动）
                 "revert", // 回滚到上一个版本
-                "test", //增加测试
+                "test", //测试用例，包括单元测试、集成测试等
                 "perf", // 优化相关，比如提升性能、体验
             ],
         ],
-        "type-case": [0],
-        "type-empty": [0],
-        "scope-empty": [0],
-        "scope-case": [0],
-        "subject-full-stop": [0, "never"],
-        "subject-case": [0, "never"],
-        "header-max-length": [0, "always", 72],
+        // "type-case": [0],
+        // "type-empty": [0],
+        // "scope-empty": [0],
+        // "scope-case": [0],
+        // "subject-full-stop": [0, "never"],
+        // "subject-case": [0, "never"],
+        // "header-max-length": [0, "always", 72],
     },
 };
