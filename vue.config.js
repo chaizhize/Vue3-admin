a9280fa715edf76b3033398e5d04a59df82ea61d
chaizhize <chaizhize>
2020-12-08 13:27:37 +0800

    upd: update

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,12 @@
 /*
  * @Author: your name
  * @Date: 2020-12-01 10:42:34
- * @LastEditTime: 2020-12-08 10:57:34
+ * @LastEditTime: 2020-12-08 13:26:13
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /vue-goodsku/vue.config.js
  */
-const cdnDomain = '//www.chaizz.cn/vue3';
+const cdnDomain = '//www.chaizz.cn/vue3/';
 module.exports = {
     publicPath: process.env.ENV === 'production' ? cdnDomain : '/', //resolve('dist'),// 基本路径
     lintOnSave: false
